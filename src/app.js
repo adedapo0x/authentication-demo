c7117b221458399bb33800de541103cfb9526fe6
Adeyemi Adedapo <adedapo025@gmail.com>
2024-06-23 23:59:12 +0100

    Setting up routes

--- src/app.js
+++ src/app.js
@@ -1,10 +1,14 @@
 const express = require('express')
+const authRouter = require('./routes/auth.routes')
+const userRouter =  require('./routes/users.routes')
 
 const app = express()
 
-
-
 app.use(express.json())
+
+app.use('/user', userRouter)
+app.use('', authRouter)
+
 
 
 
--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -1,1 +1,11 @@
 const express = require('express')
+const userRouter = express.Router()
+const {httpRegister, getUsers} = require('../controllers/users.controllers')
+const verifyJWT = require('../middlewares/verifyJWT')
+
+
+userRouter.post('/register', httpRegister)
+userRouter.get('/users', verifyJWT, getUsers)
+
+
+module.exports = userRouter
